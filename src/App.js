bb826983b32f9d9b4ecaf386f96ba47fdc347c92
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-12-08 20:47:26 -0700

    images and app base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
+import whiskey from "./images/whiskey.jpg";
+import tubby from "./images/tubby.jpg";
+import duke from "./images/duke.jpg";
+import perry from "./images/perry.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pet Matcher</h1>
+        <Dog dogs={App.defaultProps} />
       </header>
     </div>
   );
---
+++ src/Dog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Dog.css'
+
+
+const Dog = (dogs) => {
+
+    return (
+      <div>
+        <div>
+          <h3>dogs.name</h3>
+          <img src=dog.src></img>
+        </div>
+      </div>
+    );
+}
+
+export default Dog;
